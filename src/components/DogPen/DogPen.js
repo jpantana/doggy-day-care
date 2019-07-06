eb7d3a81b8ac3fb281a9235e0aa95b443e5112ca
JPantana <joshpantana@me.com>
2019-07-06 12:39:18 -0500

    added proptypes and fixed some stying.

--- src/components/DogPen/DogPen.js
+++ src/components/DogPen/DogPen.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Dog from '../Dog/Dog';
+import dogShape from '../../helpers/propz/dogShape';
 
 import './DogPen.scss';
 
 class DogPen extends React.Component {
-
+  static propTypes = {
+    dogs: PropTypes.arrayOf(dogShape.dogShape),
+  }
   render() {
-    // const { dogs } = this.props;
     const makeDogs = this.props.dogs.map(dog => (
       <Dog key={ dog.id } dog={ dog } /> // for each dog return a Dog component that returns a dog component with a prop of the dog
     ));
